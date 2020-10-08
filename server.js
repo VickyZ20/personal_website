5b9c91368937c995c421f62981e16d649b70d0a8
VickyZ20 <59904377+VickyZ20@users.noreply.github.com>
2020-10-07 20:14:34 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+//cause I am learning to connect js with http-server now, 
+//this page is great! seems like it uses json file and
+//a local port to retirve data.
+
 //middlewares
 app.use(express.json());
 app.use(express.static("express"));
